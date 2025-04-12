589dd1668abf9327f9392dd13f29eb6cdba0fafa
varungautam1411 <archervg@gmail.com>
2025-04-11 18:11:33 -0700

    Update MovieDetailsService.java

--- src/main/java/com/movierecommender/service/MovieDetailsService.java
+++ src/main/java/com/movierecommender/service/MovieDetailsService.java
@@ -1,1 +1,20 @@
+package com.movierecommender.service;
 
+import com.movierecommender.model.domain.MovieDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieDetailsService {
+    
+    public MovieDetails getMovieDetails(String movieId) {
+        // Implement this method to fetch movie details from your database
+        // This is a dummy implementation
+        MovieDetails details = new MovieDetails();
+        details.setMovieId(movieId);
+        details.setTitle("Sample Movie");
+        details.setYearOfRelease(2023);
+        return details;
+    }
+}
+
+    
