72712c265e7b40df62e63ecc070bc8e7541589ba
varungautam1411 <archervg@gmail.com>
2025-04-11 18:03:27 -0700

    Update MovieDetails.java

--- src/main/java/com/movierecommender/model/domain/MovieDetails.java
+++ src/main/java/com/movierecommender/model/domain/MovieDetails.java
@@ -1,1 +1,12 @@
+package com.movierecommender.model.domain;
 
+import lombok.Data;
+
+@Data
+public class MovieDetails {
+    private String movieId;
+    private String title;
+    private int yearOfRelease;
+}
+
+    
