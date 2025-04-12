662c69ab6fdf232363783f7d04568b99e1ad6e89
varungautam1411 <archervg@gmail.com>
2025-04-11 19:40:00 -0700

    Update WatchedMovie.java

--- src/main/java/com/movierecommender/model/domain/WatchedMovie.java
+++ src/main/java/com/movierecommender/model/domain/WatchedMovie.java
@@ -1,14 +1,49 @@
 package com.movierecommender.model.domain;
 
-import lombok.Data;
-
-@Data
 public class WatchedMovie {
     private String movieId;
     private String title;
     private int yearOfRelease;
     private int rating;
     private String date;
-}
 
-    
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
