1795c4bb78501c65ea5ad6b70f96fe96869e87b9
varungautam1411 <archervg@gmail.com>
2025-04-11 18:03:41 -0700

    Update WatchedMovie.java

--- src/main/java/com/movierecommender/model/domain/WatchedMovie.java
+++ src/main/java/com/movierecommender/model/domain/WatchedMovie.java
@@ -1,1 +1,14 @@
+package com.movierecommender.model.domain;
 
+import lombok.Data;
+
+@Data
+public class WatchedMovie {
+    private String movieId;
+    private String title;
+    private int yearOfRelease;
+    private int rating;
+    private String date;
+}
+
+    
