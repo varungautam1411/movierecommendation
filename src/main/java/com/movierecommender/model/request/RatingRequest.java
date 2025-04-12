f33ff64acdaf0f6b3325b5d6ccbb5318ee1f069a
varungautam1411 <archervg@gmail.com>
2025-04-11 18:04:15 -0700

    Update RatingRequest.java

--- src/main/java/com/movierecommender/model/request/RatingRequest.java
+++ src/main/java/com/movierecommender/model/request/RatingRequest.java
@@ -1,1 +1,24 @@
+package com.movierecommender.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import javax.validation.constraints.*;
+
+@Data
+public class RatingRequest {
+    @NotBlank(message = "Customer ID is required")
+    @JsonProperty("customer-id")
+    private String customerId;
+    
+    @NotBlank(message = "Movie ID is required")
+    @JsonProperty("movie-id")
+    private String movieId;
+    
+    @Min(1) @Max(5)
+    private int rating;
+    
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "Date must be in format YYYY-MM-DD")
+    private String date;
+}
+
+    
