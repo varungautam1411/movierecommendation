b49ff7f2cc4ade26578c4c8bfd0f97bd37dc05b5
varungautam1411 <archervg@gmail.com>
2025-04-11 17:28:47 -0700

    Update ApiResponse.java

--- src/main/java/com/movierecommender/model/response/ApiResponse.java
+++ src/main/java/com/movierecommender/model/response/ApiResponse.java
@@ -1,1 +1,14 @@
+// src/main/java/com/movierecommender/model/ApiResponse.java
+package com.movierecommender.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private boolean success;
+    private String message;
+    private String error;
+}
 
