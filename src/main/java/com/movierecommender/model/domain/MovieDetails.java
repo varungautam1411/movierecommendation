787015547ef0bc4eaf9d65e4a513533e5a3701e0
varungautam1411 <archervg@gmail.com>
2025-04-11 19:39:42 -0700

    Update MovieDetails.java

--- src/main/java/com/movierecommender/model/domain/MovieDetails.java
+++ src/main/java/com/movierecommender/model/domain/MovieDetails.java
@@ -1,12 +1,33 @@
 package com.movierecommender.model.domain;
 
-import lombok.Data;
-
-@Data
 public class MovieDetails {
     private String movieId;
     private String title;
     private int yearOfRelease;
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
 }
 
     
