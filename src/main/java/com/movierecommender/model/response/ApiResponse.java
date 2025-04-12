643e989e8f189047324ecd97edc72a829ee8d362
varungautam1411 <archervg@gmail.com>
2025-04-11 18:29:23 -0700

    Update ApiResponse.java

--- src/main/java/com/movierecommender/model/response/ApiResponse.java
+++ src/main/java/com/movierecommender/model/response/ApiResponse.java
@@ -1,14 +1,14 @@
 package com.movierecommender.model.response;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor  // Add this annotation
 public class ApiResponse {
     private boolean success;
     private String message;
     private String error;
 }
-
-    
