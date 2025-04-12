76aae0cea808a1da62c85badaa3b6c6eb150d20b
varungautam1411 <archervg@gmail.com>
2025-04-11 17:27:26 -0700

    Update MovieRecommenderApplication.java

--- src/main/java/com/movierecommender/MovieRecommenderApplication.java
+++ src/main/java/com/movierecommender/MovieRecommenderApplication.java
@@ -1,1 +1,12 @@
+// src/main/java/com/movierecommender/MovieRecommenderApplication.java
+package com.movierecommender;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRecommenderApplication.class, args);
+    }
+}
