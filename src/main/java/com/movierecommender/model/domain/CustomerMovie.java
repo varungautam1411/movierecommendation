c524e35b69c83e438760539bf3a670dd4f8fd521
varungautam1411 <archervg@gmail.com>
2025-04-11 18:03:56 -0700

    Update CustomerMovie.java

--- src/main/java/com/movierecommender/model/domain/CustomerMovie.java
+++ src/main/java/com/movierecommender/model/domain/CustomerMovie.java
@@ -1,1 +1,12 @@
+package com.movierecommender.model.domain;
+
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class CustomerMovie {
+    private String customerId;
+    private List<WatchedMovie> watchedMovies = new ArrayList<>();
+}
 
