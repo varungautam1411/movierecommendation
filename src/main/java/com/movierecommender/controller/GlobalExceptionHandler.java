cef725c4fe7e9f37059c3a1ece50903b6b36ae10
varungautam1411 <archervg@gmail.com>
2025-04-11 18:05:44 -0700

    Update GlobalExceptionHandler.java

--- src/main/java/com/movierecommender/controller/GlobalExceptionHandler.java
+++ src/main/java/com/movierecommender/controller/GlobalExceptionHandler.java
@@ -1,1 +1,29 @@
+package com.movierecommender.controller;
 
+import com.movierecommender.model.response.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleException(Exception e) {
+        return ResponseEntity.internalServerError()
+            .body(new ApiResponse(false, "An error occurred", e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleValidationException(
+            MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getAllErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest()
+            .body(new ApiResponse(false, "Validation error", errorMessage));
+    }
+}
