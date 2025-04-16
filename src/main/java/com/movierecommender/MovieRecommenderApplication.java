af05a80878bf5fe51e319236313542cfff88bf7d
varungautam1411 <archervg@gmail.com>
2025-04-16 12:26:58 -0700

    Update MovieRecommenderApplication.java

--- src/main/java/com/movierecommender/MovieRecommenderApplication.java
+++ src/main/java/com/movierecommender/MovieRecommenderApplication.java
@@ -2,10 +2,17 @@ package com.movierecommender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 public class MovieRecommenderApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieRecommenderApplication.class, args);
     }
+    
 }
